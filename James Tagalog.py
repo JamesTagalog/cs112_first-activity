b279d4be164dffffa4b895d1ee58f69668017109
James Tagalog <136965759+JamesTagalog@users.noreply.github.com>
2023-09-13 04:43:27 -0700

    Add files via upload

---
+++ James Tagalog.py
@@ -0,0 +1,11 @@
+print("Complete Name: James Ira A.Tagalog")
+
+print("Course: BSCS")
+
+print("Section: CS1E")
+
+print("Age: 20")
+
+print("Birthday: June 5, 2003")
+
+print("Complete Home Address: Acacia Carmen CDOC")
